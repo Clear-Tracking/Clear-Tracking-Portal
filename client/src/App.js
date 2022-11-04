82b9de8f918b75bfdcac3a42e23486988cf81115
Palak <palakagarwal369@gmail.com>
2022-11-04 17:28:04 +0530

    dashboard and police login added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
+import PoliceLogin from './pages/PoliceLogin';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignUp />}></Route>
-          <Route path="login" element={<Login />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/policelogin" element={<PoliceLogin />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
