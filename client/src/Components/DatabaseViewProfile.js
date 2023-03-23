89f504c1509dc5fb0a5111db5ba57cc6f8cf6135
Palak <palakagarwal369@gmail.com>
2023-03-23 23:00:41 +0530

    changes made in police dashboard

--- client/src/Components/DatabaseViewProfile.js
+++ client/src/Components/DatabaseViewProfile.js
@@ -16,34 +16,35 @@ export default function DatabaseViewProfile() {
     
 const params=useParams()
     return (
-        <Card sx={{ display: 'flex' }} style={{boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", background: "#ffffff", borderRadius: "20px"}}>
+        <Card sx={{ display: 'flex' }} style={{boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", background: "#ffffff", borderRadius: "20px", margin:"100px"}}>
             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
             <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
-                style={{height:"500px"}}/>
+                style={{height:"607px"}}/>
             </Box>
             <Box>
                 <div>
-                    <span  style={{ fontSize: "2.5rem", fontWeight: "bold" }}>Raju Rastogi {JSON.stringify(params)}</span><br/>
-                    <span>DOB: 12 march 2010</span><br/>
-<span>Gender :Male</span><br/>
-<span>Place of Missing: thakur Complex</span><br/>
-<span>Date of Missing: 12-2-23</span><br/>
-<span>Address: This is the adderess</span><br/>
-<span>Aadhar Number: 9999 9999 9999 </span><br/>
-<span style={{fontWeight:"bold"}}>Physical Features</span><br/>
-<span>Height: 5.8 feet</span><br/>
-<span>Weight: 74 Kg</span><br/>
-<span>Complexion: Fair</span><br/>
-<span>Eye Colour: Black</span><br/>
-<span>Hair Colour: Black</span><br/>
-<span>Any face Marks: None</span><br/>
-<span>Any Disability: None</span><br/>
+                    <span  style={{ fontSize: "2.5rem", fontWeight: "bold",margin:"20px" }}>Alec Benjamin {JSON.stringify(params)}</span><br/>
+                    <span style={{fontSize:"1.05rem",margin:"20px"}}>DOB: 12 march 2010</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Gender: Male</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Place of Missing: thakur Complex</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Date of Missing: 12-2-23</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Address: This is the adderess</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Aadhar Number: 9999 9999 9999 </span><br/>
+<span style={{fontWeight:"bold", margin: "20px", fontSize:"1.5rem", paddingTop:"30px"}}>Physical Features</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Height: 5.8 feet</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Weight: 74 Kg</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Complexion: Fair</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Eye Colour: Black</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Hair Colour: Black</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Any face Marks: None</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Any Disability: None</span><br/>
 
-<span>Neared Police Station: Samta Nagar</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Neared Police Station: Samta Nagar</span><br/>
 
-<span>Guardian Name</span><br/>
-<span>Contact Number</span><br/>
-<Button variant="contained">FOUND</Button>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Guardian Name: Black Benjamin</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Guardian Name: Black Benjamin</span><br/>
+<span style={{fontSize:"1.05rem",margin:"20px"}}>Contact Number: 1234567890</span><br/>
+<Button variant="contained" style={{fontSize:"1.05rem",margin:"20px"}}>FOUND</Button>
                 </div>
             </Box>
         </Card>
--- client/src/pages/dashboardPages/UserSubmittedData.js
+++ client/src/pages/dashboardPages/UserSubmittedData.js
@@ -9,42 +9,161 @@ import Typography from '@mui/material/Typography';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
-import { Button } from '@mui/material';
+import { Button, FormControl } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import formfilldisplay from "../../assets/data/formfilldisplay.json"
 
 export default function UserSubmittedData() {
     const theme = useTheme();
-
+    console.log(formfilldisplay.firstName);
     return (
-        <Card sx={{ display: 'flex' }} style={{boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", background: "#ffffff", borderRadius: "20px"}}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
-                style={{height:"500px"}}/>
-            </Box>
-            <Box>
-                <div>
-                    <span  style={{ fontSize: "2.5rem", fontWeight: "bold" }}>Raju Rastogi</span><br/>
-                    <span>DOB: 12 march 2010</span><br/>
-<span>Gender :Male</span><br/>
-<span>Place of Missing: thakur Complex</span><br/>
-<span>Date of Missing: 12-2-23</span><br/>
-<span>Address: This is the adderess</span><br/>
-<span>Aadhar Number: 9999 9999 9999 </span><br/>
-<span style={{fontWeight:"bold"}}>Physical Features</span><br/>
-<span>Height: 5.8 feet</span><br/>
-<span>Weight: 74 Kg</span><br/>
-<span>Complexion: Fair</span><br/>
-<span>Eye Colour: Black</span><br/>
-<span>Hair Colour: Black</span><br/>
-<span>Any face Marks: None</span><br/>
-<span>Any Disability: None</span><br/>
+        <>
+            <Card sx={{ display: 'flex' }} style={{width:"95%", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", background: "#ffffff", borderRadius: "20px", margin: "30px",marginTop:"100px" }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <img src={formfilldisplay.personpic}
+                        style={{ height: "610px" }} />
+                </Box>
+                <Box component="form" style={{padding:"20px"}}
+                    sx={{
+                        "flexGrow":1,
+                        '& .MuiTextField-root': { m: 1 },
+                    }}>
+                    
 
-<span>Neared Police Station: Samta Nagar</span><br/>
+                    <form>
+                        <TextField
+                        style={{width:"48%"}}
+                            required
+                            id="outlined-required"
+                            label="Name"
+                            defaultValue={formfilldisplay.firstName + " " + formfilldisplay.lastName}
+                        />
+                        <TextField
+                        style={{width:"48%"}}
+                            required
+                            id="outlined-required"
+                            label="Gender"
+                            defaultValue={formfilldisplay.gender}
+                        /><br/>
+                        <TextField
+                        style={{width:"48%"}}
+                            required
+                            id="outlined-required"
+                            label="DOB"
+                            defaultValue={formfilldisplay.dob}
+                        />
+                         <TextField
+                            required
+                            style={{width:"48%"}}
+                            id="outlined-required"
+                            label="Aadhar Number"
+                            defaultValue={formfilldisplay.aadhar}
+                        />
+                        <br/>
 
-<span>Guardian Name</span><br/>
-<span>Contact Number</span><br/>
-<Button variant="contained">Launch FIR</Button>
-                </div>
-            </Box>
-        </Card>
+                        <TextField
+                        style={{width:"48%"}}
+                            required
+                            id="outlined-required"
+                            label="Place of Missing"
+                            defaultValue={formfilldisplay.pom}
+                        />
+                        <TextField
+                        style={{width:"48%"}}
+                            required
+                            id="outlined-required"
+                            label="Date of Missing"
+                            defaultValue={formfilldisplay.dom}
+                        />
+                        
+                       
+                        <TextField
+                            required
+                            fullWidth
+                            id="outlined-required"
+                            label="Address"
+                            maxRows={4}
+                            defaultValue={formfilldisplay.Address}
+                        />
+                        <br/><span style={{ fontWeight: "bold", marginLeft:"15px", fontSize:"1.1rem",marginTop:"10px" }}>Physical Features</span><br />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Height (cm)"
+                            defaultValue={formfilldisplay.Height}
+                        />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Weight (Kg)"
+                            defaultValue={formfilldisplay.Weight}
+                        />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Complexion"
+                            defaultValue={formfilldisplay.complexion}
+                        />
+
+                        <TextField
+                        style={{width:"23%"}}
+                            required
+                            id="outlined-required"
+                            label="Eye Colour"
+                            defaultValue={formfilldisplay.Eye}
+                        />
+                        <TextField
+                        style={{width:"23%"}}
+                            required
+                            id="outlined-required"
+                            label="Hair Colour"
+                            defaultValue={formfilldisplay.Hair}
+                        />
+                        <TextField
+                        style={{width:"23%"}}
+                            required
+                            id="outlined-required"
+                            label="Any face Marks"
+                            defaultValue={formfilldisplay.Face}
+                        />
+
+                        <TextField
+                        style={{width:"23%"}}
+                            required
+                            id="outlined-required"
+                            label="Any Disability"
+                            defaultValue={formfilldisplay.aod}
+                        />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Neared Police Station"
+                            defaultValue={formfilldisplay.neareststation}
+                        />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Guardian Name"
+                            defaultValue={formfilldisplay.Guardian}
+                        />
+                        <TextField
+                        style={{width:"31%"}}
+                            required
+                            id="outlined-required"
+                            label="Contact Number"
+                            defaultValue={formfilldisplay.Contact}
+                        /><br/>
+                        <Button variant="contained">Launch FIR</Button>
+                    </form>
+                </Box>
+
+            </Card>
+
+        </>
     );
 }
